9794c385668ccba06934314b48c8a97d7b7ea329
unknown <emeline.fratacci@curie.fr>
2025-04-29 15:57:24 +0200

    Started to implement cropping on images to stack but need more thoughts with Mathieu

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
 import argparse
-from Source.crops import generate_random_crops, crop_img
+from Source.crops import generate_random_crops, crop_img, group_images_into_stacks, generate_random_crops_from_stacks
 
 def main():
     parser = argparse.ArgumentParser(description="Main pipeline", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--crop', required=True, action='store_true', help="Activate cropping")
+    parser.add_argument('--crop',  action='store_true', help="Activate cropping")
     parser.add_argument('--input', type=str, required=True, help="Input directory")
     parser.add_argument('--output', type=str, required=True, help="Output directory")
     parser.add_argument('--pattern', nargs='+', type=str, help="Pattern to match in filenames")
     parser.add_argument('--n_files', type=int, default=10, help="Number of files to sample")
     parser.add_argument('--crop_size', type=int, default=400, help="Crop size")
     parser.add_argument('--extension', type=str, default=".tif", help="File extension to match")
+
+    parser.add_argument('--stack',  action='store_true', help="Stack images")
     args = parser.parse_args()
 
-    if args.crop:
+    if args.stack and args.crop:
+        print("Stacking images and cropping...")
+        stack_files = group_images_into_stacks(input_folder=args.input, output_folder=args.output)
+        generate_random_crops_from_stacks(stack_files, n_crops=args.n_files, pattern=args.pattern, crop_size=args.crop_size, output_dir=args.output)
+    
+    elif args.stack:
+        print("Stacking images...")
+        stack_files = group_images_into_stacks(input_folder=args.input, output_folder=args.output)
+    
+    elif args.crop:
         print("Cropping images...")
         # Generate random crops
         rfiles = generate_random_crops(input=args.input, n_files=args.n_files, patterns=args.pattern, extension=args.extension)
-        
-        # Crop images and save to the output directory
         crop_img(rfiles, output_dir=args.output, size=args.crop_size)
+    
     else:
-        # Handle the case where --crop is not provided
-        print("No cropping operation performed.")
+        # Handle the case where neither --stack nor --crop is provided
+        print("No stack or crop operation performed.")
+
 
 if __name__ == "__main__":
     main()
